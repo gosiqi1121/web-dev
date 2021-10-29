d0eee7ed9e8d2f9aa6cf1d0a192d85a26c871ebb
sunarke <624910123@qq.com>
2021-10-28 22:46:56 -0700

    fix dangerous alert from netlify

--- public/a5/js/index.js
+++ public/a5/js/index.js
@@ -135,6 +135,122 @@ console.log('Find');
 console.log(four);
 console.log(string3);
 
+const fourIndex = numberArray1.findIndex(a => a === 4);
+const string3Index = stringArray1.findIndex(a => a === 'string3');
+console.log('Find index');
+console.log(fourIndex);
+console.log(string3Index);
+
+const numbersGreaterThan2 = numberArray1.filter(a => a > 2);
+const evenNumbers = numberArray1.filter(a => a % 2 === 0);
+const oddNumbers = numberArray1.filter(a => a % 2 !== 0);
+console.log('Filter');
+console.log(numberArray1);
+console.log(numbersGreaterThan2);
+console.log(evenNumbers);
+console.log(oddNumbers);
+
+const five = 2 + 3;
+const result1 = "2 + 3 = " + five;
+console.log(result1);
+
+const result2 = `2 + 3 = ${2 + 3}`;
+console.log(result2);
+
+const username = "alice";
+const greeting1 = `Welcome home ${username}`;
+console.log(greeting1);
+
+// const loggedIn = false;
+const greeting2 = `Logged in: ${loggedIn ? "Yes" : "No"}`;
+console.log(greeting2);
+
+const init = () => {
+    console.log('Hello world from jQuery');
+    const bindById = $('#bind-by-id');
+    const bindByClass = $('.bind-by-class');
+    console.log('Binding to DOM');
+    console.log(bindById);
+    console.log(bindByClass);
+
+    const changeStyle = $('#change-style');
+    const changeStyle2 = $('.change-style');
+    changeStyle.css('color', 'red');
+    changeStyle2.css('color', 'blue');
+
+    const getIdAttr = $("#get-id-attr");
+    const id = getIdAttr.attr('id');
+    console.log(id);
+
+    const setClassAttr = $("#set-class-attr");
+    setClassAttr.attr('class', 'class-0');
+
+    const addClass1Example = $('#add-class-1');
+    addClass1Example.addClass('class-1');
+    const removeClass1Example = $('#remove-class-1');
+    removeClass1Example.removeClass('class-2');
+
+    const hideMe = $("#hide-me");
+    hideMe.hide();
+    const showMe = $("#show-me");
+    showMe.show();
+
+    const newLineItem = $("<li>Line Item 1</li>");
+    const anotherLineItem = $("<li>Line item 2</li>")
+    const ul = $("#append-new-elements");
+    ul.append(newLineItem);
+    ul.append(anotherLineItem);
+
+    const removeThisLi = $("#remove-this");
+    const emptyThisUI = $("#empty-this");
+    removeThisLi.remove();
+    emptyThisUI.empty();
+
+    const changeThisText = $("#change-this-text");
+    const changeThisHtml = $("#change-this-html");
+    changeThisText.html('New text');
+    changeThisHtml.html(`
+    <li>Line item A</li>
+    <li>Line item B</li>
+    <li>Line item C</li>
+`);
+    const child2 = $("#child-2");
+    const parent1 = $('#parent');
+    parent1.css('background-color', 'red').css('color', 'white');
+    const parent = $("#parent");
+    const child = parent.find("#child-2");
+    child.css('background-color', 'blue');
+
+    const handleClick = () => console.log("Handle click");
+    const clickable = $('.clickable');
+    clickable.click(handleClick);
+
+    const handleEventTarget = (event) => {
+        const target = event.target;
+        console.log(target);
+        $(target).css('backgroundColor', 'blue').css('color', 'white');
+    }
+    const eventTarget = $('#event-target');
+    eventTarget.click(handleEventTarget);
+
+    let hideBtn, showBtn, hideShowHeader;
+    hideBtn = $('#hide');
+    showBtn = $('#show');
+    hideShowHeader = $('#hide-show');
+    const hideHandler = () => {
+        hideShowHeader.hide();
+    }
+    const showHandler = () => {
+        hideShowHeader.show();
+    }
+    hideBtn.click(hideHandler);
+    showBtn.click(showHandler);
+    //click: call function in the parameter when click the selected part
+    
+}
+$(init);
+//PASS INIT FUNCTION TO THE global function called jQuery and its alias $
+
 
 
 console.log();
