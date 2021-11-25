35241fc95d59147de5478b61c7bddfe7631b8c7b
sunarke <624910123@qq.com>
2021-11-25 12:58:04 -0800

    listen port

--- web-dev-node/server.js
+++ web-dev-node/server.js
@@ -21,4 +21,4 @@ app.get('/hello', (req, res) => {
 require('./services/movies-service')(app);
 require('./services/tweets-service')(app);
 require('./services/profile-service')(app);
-app.listen(4000);
+app.listen(process.env.PORT || 4000);
