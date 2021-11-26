bcb2c15625298cfd40fc045b9a434a4449f34465
sunarke <624910123@qq.com>
2021-11-26 02:49:09 -0800

    separate

--- src/components/a8/Challenge/profileReducer/profileA8.js
+++ src/components/a8/Challenge/profileReducer/profileA8.js
@@ -6,14 +6,14 @@ const profileA8 = (state = information, action) => {
 
             console.log("********* fetch reducer save 1 *****************", action.profile)
             // const newState = [{
-            //     name: action.name.newName,
+            //     name: action.profile[0].name,
             //     handle: "wangsiqi",
             //     profilePicture: "jose.png",
             //     bannerPicture: "https://www.tuxedo-cat.co.uk/wp-content/uploads/2016/12/shutterstock_529826206-1.jpg?ezimgfmt=ng%3Awebp%2Fngcb1%2Frs%3Adevice%2Frscb1-1",
-            //     bio: action.bio.bio,
-            //     website: action.website.web,
-            //     location: action.location.loca,
-            //     dateOfBirth: action.dateOfBirth.birthday,
+            //     bio: action.profile[0].bio,
+            //     website: action.profile[0].website,
+            //     location: action.profile[0].location,
+            //     dateOfBirth: action.profile[0].dateOfBirth,
             //     dateJoined: "11/2021",
             //     followingCount: 999,
             //     followersCount: 180,
@@ -22,6 +22,7 @@ const profileA8 = (state = information, action) => {
 
             console.log("********* fetch reducer save 2 *****************", action.profile)
             return action.profile;
+            // return newState;
 
         case  'fetch-profile':
             console.log("********* fetch reducer fetch *****************", action.profile)
